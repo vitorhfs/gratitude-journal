e0de24e03f0d9128c76eef67fef9d0fb32dd56bd
vitorhfs <vitorhugo.fernandes12@gmail.com>
2021-02-06 17:28:22 -0300

    Migrations

---
+++ backend/src/migration/1612642808341-UserTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserTable1612642808341 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ backend/src/migration/1612642893468-PhraseTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class PhraseTable1612642893468 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
